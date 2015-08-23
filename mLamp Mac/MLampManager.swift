e163b66ff5644ad3c85678c0861990c325851557
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2015-08-23 12:57:22 +0200

    add code to load and store known identifiers

--- mLamp Mac/MLampManager.swift
+++ mLamp Mac/MLampManager.swift
@@ -17,6 +17,11 @@ public class MLampManager: RFDuinoManagerDelegate {
     public private(set) var lamps = [MLamp]()
     
     private var rfduinoManager = RFDuinoManager()
+    private var knownIdentifiers = [NSUUID: String]()
+    
+    public init() {
+        loadKnownIdentifiers()
+    }
     
     public func discover() {
         lamps.removeAll()
@@ -45,5 +50,54 @@ public class MLampManager: RFDuinoManagerDelegate {
         
         lamps.append(lamp)
         delegate?.mLampDidDiscoverLamp(lamp)
+    }
+    
+    private func loadKnownIdentifiers() {
+        knownIdentifiers.removeAll()
+        
+        guard let url = getKnownIdentifierStorageURL() else {
+            return
+        }
+        
+        guard let dict = NSDictionary(contentsOfURL: url) else {
+            NSLog("Failed to load known identifiers")
+            return
+        }
+        
+        for identifier in dict {
+            knownIdentifiers[identifier.key as! NSUUID] = (identifier.value as! String)
+        }
+    }
+    
+    private func saveKnownIdentifiers() {
+        guard let url = getKnownIdentifierStorageURL() else {
+            return
+        }
+        
+        let dict = NSMutableDictionary()
+        for identifier in knownIdentifiers {
+            dict.setValue(identifier.0.UUIDString, forKey: identifier.1)
+        }
+        
+        dict.writeToURL(url, atomically: true)
+    }
+    
+    private func getKnownIdentifierStorageURL() -> NSURL? {
+        let directory: NSURL
+        do {
+            directory = try NSFileManager.defaultManager().URLForDirectory(
+                NSSearchPathDirectory.ApplicationSupportDirectory,
+                inDomain: NSSearchPathDomainMask.UserDomainMask,
+                appropriateForURL: nil,
+                create: true)
+        } catch {
+            NSLog("Failed to find Application Support Directory")
+            return nil
+        }
+        
+        let path = directory.URLByAppendingPathComponent("known_lamps.plist")
+        NSLog("Known lamp path has been set to: %@", path)
+        
+        return path
     }
 }
